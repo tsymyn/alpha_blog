5e04cdc23f89f4010389dfca49d02e032bbb5991
Noah Erickson <noah.erickson@gmail.com>
2016-03-06 05:00:01 +0000

    changed update flash style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:success] = "Article was successfully updated"
+      flash[:info] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
